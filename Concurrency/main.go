efbc42307c91662828fd00c3a845d357b50dd745
pinklaosa <rps53009@hotmail.com>
2025-03-16 14:35:10 +0700

    Concurrent 2

--- Concurrency/main.go
+++ Concurrency/main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
-	HellloRoutine()
+	// HellloRoutine()
+	CalculateParallel()
 }
 
 func HellloRoutine() {
@@ -21,4 +22,35 @@ func HellloRoutine() {
 	}
 	wg.Wait()
 	fmt.Println("All routine done !")
+}
+
+func SumSlices(numbers []int, wg *sync.WaitGroup, ch chan<- int) {
+	defer wg.Done()
+	var sum int
+	for _, v := range numbers {
+		sum += v
+	}
+	ch <- sum
+}
+
+func CalculateParallel() {
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	var wg sync.WaitGroup
+	partSize := len(numbers) / 2
+	numbers1 := numbers[:partSize]
+	numbers2 := numbers[partSize:]
+	ch := make(chan int, 2)
+	
+	wg.Add(2)
+	go SumSlices(numbers1,&wg,ch)
+	go SumSlices(numbers2,&wg,ch)
+
+	wg.Wait()
+	close(ch)
+	
+	sum := 0
+	for  v := range ch {
+		sum += v		
+	}
+	fmt.Println(sum)
 }
